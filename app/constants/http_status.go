fad9f80449ddf3cbd0d727bbe1625b0512bcb5bc
otoshimtoshi <1083otiket@gmail.com>
2025-02-08 23:40:32 +0900

    feat: add constants

---
+++ app/constants/http_status.go
@@ -0,0 +1,14 @@
+package constants
+
+const (
+	StatusOK                  = 200
+	StatusCreated             = 201
+	StatusNoContent           = 204
+	StatusBadRequest          = 400
+	StatusUnauthorized        = 401
+	StatusForbidden           = 403
+	StatusNotFound            = 404
+	StatusConflict            = 409
+	StatusInternalServerError = 500
+	StatusServiceUnavailable  = 503
+)
